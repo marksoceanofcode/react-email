851e6ec37f6594e6b254726246ced0a346cf1f41
Mark <xenojs@outlook.com>
2025-09-11 20:01:59 -0700

    Initial commit

---
+++ emails/sample/res-test.tsx
@@ -0,0 +1,95 @@
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components';
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : '';
+
+export const StripeWelcomeEmail = () => (
+  <Html>
+    <Head />
+    <Preview>
+      Testing React.Email for email creation using React + Tailwind.
+    </Preview>
+    <Tailwind>
+      <Body style={main}>
+        <Container style={container}>
+          <Section>
+            <Heading className='font-bold text-center' as="h2">
+              Apple Event
+            </Heading>
+
+            <Img
+              className="w-full"
+              src={`${baseUrl}/static/hero_apple_event_september_2x.png`}
+              alt="Apple September Event"
+            />
+          </Section>
+
+          <Section>
+            <Heading className='font-bold text-center' as="h3">
+              9/9/2025 @ 10AM PT.
+            </Heading>
+
+            <Text className='text-center'>
+              Watch online tomorrow at 10 a.m. PT.<br/>
+              Tune in at apple.com or on the Apple TV app.
+            </Text>
+
+            <Text className='text-center'>
+              
+            </Text>
+          </Section>
+
+          {/* A simple `section` */}
+          <Section>
+            <Text>Hello World</Text>
+          </Section>
+
+          {/* Formatted with `rows` and `columns` */}
+          <Section>
+            <Row>
+              <Column>Column 1, Row 1</Column>
+              <Column>Column 2, Row 1</Column>
+            </Row>
+            <Row>
+              <Column>Column 1, Row 2</Column>
+              <Column>Column 2, Row 2</Column>
+            </Row>
+          </Section>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
+);
+
+export default StripeWelcomeEmail;
+
+const main = {
+  backgroundColor: '#000000',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
+};
+
+const container = {
+  backgroundColor: '#000000',
+  color: '#f5f5f7',
+  margin: '0 auto',
+  padding: '20px 0 48px',
+  marginBottom: '64px',
+};
