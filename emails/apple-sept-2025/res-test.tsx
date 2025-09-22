2e56f2bf99ba871353993ee6d38fd84a8b63d3d1
Mark <xenojs@outlook.com>
2025-09-21 21:40:29 -0700

    updates to email - added a button link

--- emails/apple-sept-2025/res-test.tsx
+++ emails/apple-sept-2025/res-test.tsx
@@ -34,11 +34,12 @@ export const StripeWelcomeEmail = () => (
               Apple Event<br/>
             </Heading> */}
             <Text className='font-bold text-center text-3xl'>
-              Apple Event
+              Apple Event<br/>
+              September 2025
             </Text>
 
             <Img
-              className="w-full"
+              className="max-w-96 mx-auto w-full"
               src={`${baseUrl}/static/apple-sept-2025/hero_apple_event_september_2x.png`}
               alt="Apple September Event"
             />
@@ -48,32 +49,28 @@ export const StripeWelcomeEmail = () => (
             {/* <Heading className='font-bold text-center' as="h3">
               Recap
             </Heading> */}
-            <Text className='font-semibold text-center text-xl'>
+            {/* <Text className='font-semibold text-center text-2xl'>
               Recap
-            </Text>
+            </Text> */}
 
-            <Text className='font-normal text-base text-center'>
+            <Text className='font-normal text-sm text-center'>
               Catch-up on all the latest from the September 2025 Apple event.<br/>
               Meet the new iPhone 17, iPhone 17 Pro,
-              iPhone Air, Apple Watch, and Air Pods Pro.
-            </Text>
-
-            <Text className='text-center'>
-              
+              iPhone Air, Apple Watch Series 11, and Air Pods Pro 3.
             </Text>
           </Container>
 
-          {/* Formatted with `rows` and `columns` */}
-          <Section>
-            <Row>
-              <Column>Column 1, Row 1</Column>
-              <Column>Column 2, Row 1</Column>
-            </Row>
-            <Row>
-              <Column>Column 1, Row 2</Column>
-              <Column>Column 2, Row 2</Column>
-            </Row>
-          </Section>
+          <Container className='font-semibold pt-8 text-center'>
+            <Button
+              href="https://apple.com"
+              className='text-center text-white'
+              style={btnOutline}
+            >
+              Explore
+            </Button>
+          </Container>
+          
+          
         </Container>
       </Body>
     </Tailwind>
@@ -92,6 +89,14 @@ const container = {
   backgroundColor: '#000000',
   color: '#f5f5f7',
   margin: '0 auto',
+  marginBottom: '32px',
+  maxWidth: '600px',
   padding: '20px 0 48px',
-  marginBottom: '64px',
 };
+
+const btnOutline = {
+  border: '2px solid #ffffff',
+  borderRadius: '24px',
+  color: '#ffffff',
+  padding: '12px 32px'
+}
